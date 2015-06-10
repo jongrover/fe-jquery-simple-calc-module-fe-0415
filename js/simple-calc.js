936b5599be59e693c967be1f0665e0319675cba2
jongrover <hello@jonathangrover.com>
2015-06-10 11:17:45 -0400

    solve functional solution

--- js/simple-calc.js
+++ js/simple-calc.js
@@ -1,141 +1,89 @@
-$(function(){
-
-	var counterOne = 0;
-	var counterTwo = 0;
-	var answer = 0;
-	
-
-
-	// COUNTER 1
-	$('#number1 button:eq(0)').click(function() {
-
-		counterOne += 1;
-		$('#number1 h2').text(counterOne);
-
-    });
-
-    $('#number1 button:eq(1)').click(function() {
-    	if (counterOne > 0) {
-		counterOne -= 1;
-		$('#number1 h2').text(counterOne);
-
-	}
-
-    });
-
-    // COUNTER 2
-
-    $('#number2 button:eq(0)').click(function() {
-    	counterTwo += 1;
-    	$('#number2 h2').text(counterTwo);
-
-
-
-
-    });
-
-    $('#number2 button:eq(1)').click(function(){
-    	if (counterTwo > 0) {
-    		counterTwo -= 1;
-    		$('#number2 h2').text(counterTwo);
-
-
-
-    	}
-
-
-
-    });
-
-
-    $('#add').click(function(){
-    	$('#operation').text('+');
-
-
-
-    });
-
-     $('#sub').click(function(){
-    	$('#operation').text('-');
-
-
-
-    });
-
-      $('#mult').click(function(){
-    	$('#operation').text('*');
-
-
-
-     });
-
-       $('#div').click(function(){
-    	$('#operation').text('/');
-
-
-
-    });
-
-    //   OPTION FOR RESULT USING SWITCH STATEMENT 
-  // $('#equals').click(function(){  
-    // switch(operation) {
-    //   case '+': answer = counterOne + counterTwo; $('#result').text(answer); break;
-    //   case '-': answer = counterOne - counterTwo; $('#result').text(answer); break;
-    //   case '*': answer = counterOne * counterTwo; $('#result').text(answer); break;
-    //   case '/': answer = counterOne / counterTwo; $('#result').text(answer); break;
-    //   default : answer = NaN; $('#result').text(answer);
-    // }
-    // });
-  
-    //  OPTION FOR RESULT USING IF ELSE STATEMENT
-
-  $('#equals').click(function(){  
-  if (operation = "+") { // calculate based on the operator
-
-        	answer = counterOne + counterTwo;// get the sum from counters 1 and 2 
-        	$('#result').text(answer);//display the result
-        }   
-
-        else  (operation = "-") { // calculate based on the operator
-
-        	answer = counterOne - counterTwo;// get the sum from counters 1 and 2 
-        	$('#result').text(answer);//display the result
-
-         else  (operation = "*") { // calculate based on the operator
-
-        	answer = counterOne * counterTwo;// get the sum from counters 1 and 2 
-        	$('#result').text(answer);//display the result
-        	
-         else  (operation = "/") { // calculate based on the operator
-
-        	answer = counterOne / counterTwo;// get the sum from counters 1 and 2 
-        	$('#result').text(answer);//display the result		
-
-
-
-       
-
-});
-       
-
-
-
-
-    
-
-
-
-
-
-
-      
-
-
-
-
-    
-
-
-  // write your solution here.
+$(function () {
+
+  var num1         =  0,
+	    num2         =  0,
+	    answer       =  0,
+      operation    =  '+',
+      $num1Incr    =  $('#number1 button:eq(0)'),
+      $num1Decr    =  $('#number1 button:eq(1)'),
+      $num1Display =  $('#number1 h2'),
+      $num2Incr    =  $('#number2 button:eq(0)'),
+      $num2Decr    =  $('#number2 button:eq(1)'),
+      $num2Display =  $('#number2 h2'),
+      $add         =  $('#add'),
+      $sub         =  $('#sub'),
+      $mult        =  $('#mult'),
+      $div         =  $('#div'),
+      $operation   =  $('#operation'),
+      $equals      =  $('#equals'),
+      $result      =  $('#result');
+
+  function print(sel, value) {
+    sel.text(value);
+  }
+
+	$num1Incr.click(function () {
+		num1 += 1;
+		print($num1Display, num1);
+  });
+
+  $num1Decr.click(function () {
+    if (num1 > 0) {
+		  num1 -= 1;
+		  print($num1Display, num1);
+    }
+  });
+
+  $num2Incr.click(function () {
+  	num2 += 1;
+  	print($num2Display, num2);
+  });
+
+  $num2Decr.click(function () {
+  	if (num2 > 0) {
+  		num2 -= 1;
+  		print($num2Display, num2);
+  	}
+  });
+
+
+  $add.click(function () {
+    operation = '+';
+  	print($operation, operation);
+  });
+
+  $sub.click(function () {
+    operation = '-';
+    print($operation, operation);
+  });
+
+  $mult.click(function () {
+    operation = '*';
+    print($operation, operation);
+  });
+
+  $div.click(function () {
+    operation = '/';
+    print($operation, operation);
+  });
+
+  $equals.click(function () {
+    if (operation === "+") { 
+    	answer = num1 + num2; 
+    	print($result, answer); 
+    }   
+    else if (operation === "-") { 
+    	answer = num1 - num2; 
+    	print($result, answer); 
+    }
+    else if (operation === "*") { 
+    	answer = num1 * num2; 
+    	print($result, answer); 
+    }
+    else if (operation === "/") { 
+    	answer = num1 / num2; 
+    	print($result, answer);
+    }
+  }); 
 
 });
